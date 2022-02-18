e7181e3affb06c210270fe6ea3e5512b990d25ee
HOSein KH <hkh659@gmail.com>
2022-02-18 15:07:29 +0330

    add babel to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const hossein = {
-    age: 29,
-    gender: "male",
-    developer: true,
-}
+  name: "Hossein",
+  age: 29,
+  gender: "male",
+  developer: true,
+};
+const ryan = {
+  name: "Ryan",
+  age: 3,
+  gender: "male",
+  sonOf: { ...hossein },
+};
 console.log(hossein);
+console.log(ryan);
