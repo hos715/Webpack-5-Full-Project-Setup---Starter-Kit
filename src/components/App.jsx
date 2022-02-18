ed55e62beb00c6f3989b5f0cbe0b5e6a93515411
HOSein KH <hkh659@gmail.com>
2022-02-18 20:12:27 +0330

    add image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "../styles/index.scss";
 import Persons from "./persons";
-
+import webPackImage from "../images/wbpack.png";
 const App = () => {
   return (
     <>
       <section className="hero"></section>
+
       <main>
         <section>
           <h1>hi react</h1>
         </section>
+        <img src={webPackImage} width="150" alt="webpack-log" />
         <Persons />
       </main>
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,24 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
   target,
-
+  output: {
+    assetModuleFilename: "images/[hash][ext]",
+  },
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 32 * 640,
+          },
+        },
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          { loader: MiniCssExtractPlugin.loader, options: { publicPath: "" } },
           "css-loader",
           "postcss-loader",
           "sass-loader",
